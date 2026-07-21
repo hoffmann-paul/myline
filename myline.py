d66778feba413526e608be962368ffe099acc70f
Paul Hoffmann <paulhoffmann410@gmail.com>
2026-07-21 15:19:00 +0200

    Add functions to check and force kill MyLine process

--- myline.py
+++ myline.py
@@ -288,6 +288,29 @@ def myline_help_info(flags):
     Wprint("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.")
     Wprint("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.")
 
+def myline_check_changes(flags):
+    with open(file_data_json, 'r') as file:
+        saved_data = json.load(file)
+    if saved_data != data:
+        Rprint("Unsaved Changes between data and data.json")
+    else:
+        Gprint("No Unsaved Changes")
+
+def myline_kill_check(flags):
+    with open(file_data_json, 'r') as file:
+        saved_data = json.load(file)
+    if saved_data != data:
+        Rprint("Unsaved Changes between data and data.json")
+        Rprint("Killing process is canceld...")
+    else:
+        Gprint("No Unsaved Changes")
+        RRprint("Kill MyLine...")
+        sys.exit()
+
+def myline_kill_force(flags):
+    RRprint("Kill MyLine...")
+    sys.exit()
+
 commands = {
     "data": {
         "GET": {
@@ -322,6 +345,13 @@ commands = {
         "help": {
             "c": myline_help_c,
             "info": myline_help_info
+        },
+        "check": {
+            "changes": myline_check_changes
+        },
+        "kill": {
+            "check": myline_kill_check,
+            "force": myline_kill_force
         }
     }
 } 
@@ -352,4 +382,5 @@ while True:
                 RRprint(f">>{raw}<< isnt't a vaild command")
     except Exception as e:
             RRprint(f"Unexpected Error: {e}")
+            RRprint(f"Please open up a Issue on Github")
     Wprint("")
