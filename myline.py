757d533fe91dd7401a2db02c91bbaa0d0b454382
Mohammed Anas Nathani <mohammedanasnathani0123@gmail.com>
2026-07-27 07:36:48 +0530

    fix: handle missing data.json in check changes (#86)

--- myline.py
+++ myline.py
@@ -512,8 +512,12 @@ def myline_help_info(flags):
     Wprint("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.")
 
 def myline_check_changes(flags):
-    with open(file_data_json, 'r') as file:
-        saved_data = json.load(file)
+    try:
+        with open(file_data_json, 'r') as file:
+            saved_data = json.load(file)
+    except Exception:
+        Rprint(f"Can't read {file_data_json} to check for unsaved changes")
+        return
     if saved_data != data:
         Rprint("Unsaved Changes between data and data.json")
     else:
