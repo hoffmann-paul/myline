ae07f861db2e06cded18a09c823678e062c36b84
Paul Hoffmann <paulhoffmann410@gmail.com>
2026-08-26 19:54:41 +0200

    Created Basic Struc (Again)

---
+++ MyLine Mobile/MyLine Mobile/MyLine_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLine_MobileApp.swift
+//  MyLine Mobile
+//
+//  Created by Paul Hoffmann on 26.08.26.
+//
+
+import SwiftUI
+
+@main
+struct MyLine_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
