e5e7efb8f5584d6459853f50c3252d679e60493b
Paul Hoffmann <paulhoffmann410@gmail.com>
2026-08-26 20:10:32 +0200

    Created Buttons for import and export data

--- MyLine Mobile/MyLine Mobile/Settings.swift
+++ MyLine Mobile/MyLine Mobile/Settings.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct Settings: View {
+    
+    func import_data() {
+        
+    }
+    
+    func export_data() {
+        
+    }
+    
     var body: some View {
         VStack() {
+            Button("Import Data") {
+                import_data()
+            }
+            .frame(width: 200, height: 50)
+            .background(Color.accentColor)
+            .cornerRadius(10)
+            .foregroundStyle(Color.black)
+            
+            Button("Export Data") {
+                export_data()
+            }
+            .frame(width: 200, height: 50)
+            .background(Color.accentColor)
+            .cornerRadius(10)
+            .foregroundStyle(Color.black)
+            
+            
             Spacer()
         }
     }
